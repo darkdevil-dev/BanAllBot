ca154a04270e2ffd4952a3addbf1d6a3dab188ca
Dark Devil <123370768+darkdevil-dev@users.noreply.github.com>
2025-01-27 18:30:53 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ class Config:
     API_ID=26696077
     API_HASH="5bcb1081ae8d83ac6f9669cea986b6c5"
     TOKEN="7214089636:AAEvK-kD0i1XjCnZin0Ph1DgTDgiFOkH0kQ"
-    SUDO = list(int(i) for i in os.environ.get("SUDO", "7534773118", "7966870768").split(" "))
+    SUDO = list(int(i) for i in os.environ.get("SUDO", "7534773118").split(" "))
     START_IMG="https://telegra.ph/file/52fefb8bd51289a83a49b.jpg"
     BOT_ID=7214089636
     BOT_USERNAME="SHIHAO_ROBOT"
