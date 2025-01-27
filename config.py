af10299f1e5139ce8a087f6cd9d31298e74f763c
Dark Devil <123370768+darkdevil-dev@users.noreply.github.com>
2025-01-27 18:10:31 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,10 +1,10 @@
 import os
 class Config:
-    API_ID=24509589
-    API_HASH="717cf21d94c4934bcbe1eaa1ad86ae75"
-    TOKEN="8084144216:AAEKmJkS-jwFFlmAtwvJaCoNQlzv1s5FJkQ"
-    SUDO = list(int(i) for i in os.environ.get("SUDO", "8158464263").split(" "))
+    API_ID=26696077
+    API_HASH="5bcb1081ae8d83ac6f9669cea986b6c5"
+    TOKEN="7214089636:AAEvK-kD0i1XjCnZin0Ph1DgTDgiFOkH0kQ"
+    SUDO = list(int(i) for i in os.environ.get("SUDO", "7534773118", "7966870768").split(" "))
     START_IMG="https://telegra.ph/file/52fefb8bd51289a83a49b.jpg"
-    BOT_ID=8084144216
-    BOT_USERNAME="ZuliaiBot"
-    BOT_NAME="ZuliaiBot"
+    BOT_ID=7214089636
+    BOT_USERNAME="SHIHAO_ROBOT"
+    BOT_NAME="完美世界 石昊"
