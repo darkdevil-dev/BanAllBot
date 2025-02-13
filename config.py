460fd237b14adccdbf97961281f079502288f88b
Dark Devil <123370768+darkdevil-dev@users.noreply.github.com>
2025-02-13 11:47:11 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,9 +1,9 @@
 import os
 
 class Config:
-    API_ID = 27642480
-    API_HASH = "be786a8240abf6e12283173515e2f833"
-    TOKEN = "7880134209:AAHzZbaAvww-xP0CbV_23wW6cQl6cO2vfJg"
+    API_ID = 
+    API_HASH = ""
+    TOKEN = ""
     SUDO_RAW = os.environ.get("SUDO", "7644357019,7263027158")  # Default value
     SUDO = [int(i) for i in SUDO_RAW.replace(",", " ").split() if i.strip().isdigit()]
     START_IMG = "https://telegra.ph/file/52fefb8bd51289a83a49b.jpg"
