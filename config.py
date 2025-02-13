c8ae3d37fd96c1ab69682a925d833f86695ac110
Dark Devil <123370768+darkdevil-dev@users.noreply.github.com>
2025-02-13 11:20:26 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,12 +1,12 @@
 import os
 
 class Config:
-    API_ID = 26696077
-    API_HASH = ""
-    TOKEN = ""
-    SUDO_RAW = os.environ.get("SUDO", "7644357019,7534773118")  # Default value
+    API_ID = 27642480
+    API_HASH = "be786a8240abf6e12283173515e2f833"
+    TOKEN = "7880134209:AAHzZbaAvww-xP0CbV_23wW6cQl6cO2vfJg"
+    SUDO_RAW = os.environ.get("SUDO", "7644357019,7263027158")  # Default value
     SUDO = [int(i) for i in SUDO_RAW.replace(",", " ").split() if i.strip().isdigit()]
     START_IMG = "https://telegra.ph/file/52fefb8bd51289a83a49b.jpg"
-    BOT_ID = 
-    BOT_USERNAME = "SHIHAO_ROBOT"
+    BOT_ID = 7880134209
+    BOT_USERNAME = "Musics_ProRobot"
     BOT_NAME = "完美世界 石昊"
